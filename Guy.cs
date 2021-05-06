b4b9ed06b7c53973102f38e4896e11806278290b
Robin Blom <robin.blom@hotmail.se>
2021-05-06 14:32:31 +0200

    Add project files.

---
+++ Guy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingGame
+{
+    class Guy
+    {
+        public string Name;             // The Name and the Cash fields keep track of the guy's name and how much cash he has in his pocket. 
+        public int Cash;
+
+        /// <summary>
+        /// Writes my name and the amount of cash i have to the console
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine($"The player has {Cash} bucks.");             // Sometimes you want to ask an object to perform a task, like printing a description of itself to the console. 
+        }
+
+                                                                          
+
+        /// <summary>
+        /// Gives some of my cash, removing it from my wallet (or printing a message to the console if i don't have enough cash)
+        /// </summary>
+        /// <param name="amount">Amount of cash to give</param>
+        /// <returns>The amount of cash removed from my wallet, or 0 if i don't have enough cash (or if the amount is invalid)</returns>
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)                // The method verifies that the amount they're being asked to give is valid. That way you can't ask a guy to receive a negative number, which would cause him to lose cash.
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't a valid amount!");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine($"{Name} says: I don't have enough cash to give you {amount}!");
+                return 0;
+            }
+            Cash -= amount;         // The amount specified by the user gets subtracted from the specific Guys Cash-variable.
+            return amount;          // The amount then gets returned to where the method was called from. 
+        }
+
+        /// <summary>
+        /// Recieve some cash, adding it to my wallet (or printing a message to the console if the amount is invalid)
+        /// </summary>
+        /// <param name="amount">Amount of cash to give</param>
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0)                // The method verifies that the amount they're being asked to receive is valid. That way you can't ask a guy to receive a negative number, which would cause him to lose cash.
+            {
+                Console.WriteLine($"{Name} says: {amount} isn't an amount i'll take!");
+            }
+            else
+            {
+                Cash += amount;     // The amount specified by the user gets added to the specific Guys Cash-variable. 
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = .75;
+
+            Guy player = new Guy() { Name = "The Player", Cash = 100 };
+
+            Console.WriteLine($"Welcome to the Casino. The odds are: {odds}");
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();                                       // For every iteration of the loop we print the players current amout of cash in the wallet. 
+
+                Console.Write("How much do you want to bet: ");
+                string howMuch = Console.ReadLine();
+
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = player.GiveCash(amount) * 2;                  // If the convertion by the TryParse is successfull, the amount gets subtracted from the players wallet. It then gets multiplied by two, because it's a double-or-nothing bet, and then it gets put in the pot. 
+
+                    if (pot > 0)                                            // If the pot is bigger then 0 (if the player added anything to the pot)
+                    {
+                        if (random.NextDouble() > odds)                     // If the random number (between 0.0 - 1.0) is bigger than the odds (0.75) 
+                        {
+                            int winnings = pot;                             // The player has won! And receives the winnings by a call to the ReceiveCash-method. 
+                            Console.WriteLine($"You win {winnings}");
+                            player.ReceiveCash(winnings);
+                        }
+                        else                                                // Else, if the odds is larger than the random number...
+                        {
+                            Console.WriteLine("Bad luck, you loose.");      // The player has lost!
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number.");      // This line gets printed if the convertion by the TryParse-method was unsuccessfull.
+                }
+            }
+            Console.WriteLine("The house always wins!");                    // This line gets printed when the program exits the loop, with other words, when the player has 0 cash left. 
+
+
+
+        }
+    }
+}
